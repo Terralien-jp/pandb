0f45357f1064543726f1ad68944d60d960e1453f
Terralien-jp <terralienjp@gmail.com>
2022-03-24 20:02:07 +0900

    fix foreach

--- stockList.php
+++ stockList.php
@@ -94,30 +94,20 @@
                     備考
                 </th>
             </tr>
+<?php
+    foreach( $stock_data as $val ) {
+?>
             <tr>
-                <td class="num"><?= $stock_data[0]["stock_id"] ?></td>
-                <td><?= $stock_data[0]["stock_name"] ?></td>
-                <td class="num"><?= $stock_data[0]["amount"] ?></td>
+                <td class="num"><?= $val["stock_id"] ?></td>
+                <td><?= $val["stock_name"] ?></td>
+                <td class="num"><?= $val["amount"] ?></td>
                 <td>パン生地の材料</td>
-                <td class="num"><?= $stock_data[0]["n"] ?></td>
-                <td><?= $stock_data[0]["remarks"] ?></td>
+                <td class="num"><?= $val["n"] ?></td>
+                <td><?= $val["remarks"] ?></td>
             </tr>
-            <tr>
-            <td class="num"><?= $stock_data[1]["stock_id"] ?></td>
-                <td><?= $stock_data[1]["stock_name"] ?></td>
-                <td class="num"><?= $stock_data[1]["amount"] ?></td>
-                <td>パン生地の材料</td>
-                <td class="num"><?= $stock_data[1]["n"] ?></td>
-                <td><?= $stock_data[1]["remarks"] ?></td>
-            </tr>
-            <tr>
-            <td class="num"><?= $stock_data[2]["stock_id"] ?></td>
-                <td><?= $stock_data[2]["stock_name"] ?></td>
-                <td class="num"><?= $stock_data[2]["amount"] ?></td>
-                <td>パン生地の材料</td>
-                <td class="num"><?= $stock_data[2]["n"] ?></td>
-                <td><?= $stock_data[2]["remarks"] ?></td>
-            </tr>
+<?php
+    }
+?>
         </table>
     </div>
         <div id="footer">Copylight © All Right Reserved By JMOOC</div>
