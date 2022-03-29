169633da7c8ca0a1c86219baa5edc780a1059d16
Terralien-jp <terralienjp@gmail.com>
2022-03-29 16:54:51 +0900

    fix typo

--- dbTest.php
+++ dbTest.php
@@ -13,6 +13,28 @@
     else {
         echo "connect OK";
     }
-    mysql_close($conn);
+
+// $sql ="INSERT INTO stockUser (user_number, user_name, user_password, enable)";
+// $sql ="VALUES ('A13', '毘沙門太郎', '111', 1)";
+//     if($result = mysqli_query($conn, $sql)){
+//         echo "insert OK<br>";
+//     }
+//     else {
+//         echo "insert NG<br>";
+//     }
+
+$sql = "SELECT * FROM stockUser";
+
+    if($result = mysqli_query($conn, $sql)){
+        echo "select OK<br>";
+        echo "レコード件数" . mysqli_num_rows($result) . "<br>";
+    }
+    else {
+        echo "select NG";
+    }
+while($row = mysqli_fetch_assoc($result)){
+    echo $row['user_id'] . "," .$row['user_number'] . "," .$row['user_name'] . "," . $row["user_password"] . "<br>";
+}
+    mysqli_close($conn);
     ?>
     finish
