858b4b093d67c7273e10d4f834a31079fe92d30a
Terralien-jp <terralienjp@gmail.com>
2022-03-31 19:16:55 +0900

    fix userCheck.php

--- userCheck.php
+++ userCheck.php
@@ -1,31 +1,31 @@
-<?php
-    $flag = true;
-    if($_POST["user_number"]==""){
-        $flag = false;
-    }
+if($flag) {
+    include "dbConnect.php";
 
-    if($_POST["user_name"]==""){
-        $flag = false;
-    }
+    $user_number = $_POST["user_number"];
+    $user_name = $_POST["user_name"];
+    $user_password = $_POST["user_password"];
 
-    if($_POST["user_password"]==""){
-        $flag = false;
-    }
+    $sql = "INSERT INTO stockUser (user_number, user_name, user_password, enable)";
+    $sql .= "VALUES('$user_number', '$user_name', '$user_password', 1)";
+    echo $sql;
 
-    if($_POST["user_password"]!=$_POST["user_password2"]){
-        $flag = false;
-    }
-    
-    $host = $_SERVER['HTTP_HOST'];
-    $uri = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-
-    if($flag) {
-        $extra = "stockList.php";
-        header("Location:http://$host$uri/$extra");
+    if ($result = mysqli_query($conn, $sql)){
+        echo "insert OK<br>";
     }
     else {
-        $extra = "userRegister.html";
-        header("Location:http://$host$uri/$extra");
-        exit();        
+        echo "insert NG<br>";
     }
-?>
+
+    $sql ="select * from stockUser";
+    if($result = mysqli_query($conn, $sql)){
+        echo "select OK<br>";
+    }
+    else {
+        echo "select NG<br>";
+    }
+
+    while($row = mysqli_fetch_assoc($result)) {
+        echo $row['user_id'] . "," . $row['user_number'] . "," . $row['user_name'] . "," . $row['user_password'] . "<br>";
+    }
+    mysqli_close($conn);
+}
